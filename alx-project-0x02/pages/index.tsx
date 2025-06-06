9c6202a5f235bb4c6e7c3ecfde9db524d879bc77
bemnet-21 <anjejobemnet@gmail.com>
2025-06-07 01:14:58 +0300

    renamed a directory

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index: React.FC = () => {
+  return (
+    <div className='p-8 shadow-lg bg-white text-gray-600 w-50'>Welcome</div>
+  )
+}
+
+export default index
